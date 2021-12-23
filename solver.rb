a6d0ecd2b92891011a6dcb7a1ea100c5169467fe
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-12-23 23:21:18 +0300

    Make test cases pass for fizzbuzz

--- solver.rb
+++ solver.rb
@@ -7,4 +7,16 @@ class Solver
       return num * factorial(num - 1)
     end
   end 
+
+  def fizzbuzz(num)
+     if num % 5 == 0 && num % 3 == 0
+      'fizzbuzz'
+     elsif num % 3 == 0
+      'fizz'
+     elsif num % 5 == 0
+      'buzz'
+     else 
+      "#{num}"
+     end
+  end
 end
